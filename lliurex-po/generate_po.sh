bff4ca8149c0f8f9972dc78a5db95efe741076cc
jrpelegrina <juapesai@hotmail.com>
2018-10-03 17:09:57 +0200

    Ported from Xenial

---
+++ lliurex-po/generate_po.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xgettext --join-existing ../install-files/usr/share/lliurex-java-panel/lliurex-java-panel.py ../install-files/usr/share/lliurex-java-panel/rsrc/lliurex-java-panel.ui -o ./lliurex-java-panel/lliurex-java-panel.pot
